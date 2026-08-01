8f1c9232fde5295d6a8209f0ca262b0cb6ff032a
Madhav189 <chmadhavprakash@gmail.com>
2026-08-01 13:37:19 +0530

    feat: integrated FAISS and HuggingFace embeddings

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
 
 def process_pdf(file_path):
     loader = PyPDFLoader(file_path)
@@ -11,3 +13,9 @@ def process_pdf(file_path):
     )
     chunks = text_splitter.split_documents(docs)
     return chunks
+
+def create_vector_store(chunks):
+    embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+    vector_store = FAISS.from_documents(chunks, embeddings)
+    vector_store.save_local("faiss_index")
+    return vector_store
